52da51906b17a4039000dc36d3614906b719a57f
orielo <orielovadia@gmail.com>
2018-10-17 16:36:59 +0300

    Update ps.py

--- ps.py
+++ ps.py
@@ -7,6 +7,7 @@ import psutil
 import click
 from python_toolbox import caching
 
+
 @caching.cache()
 def get_cmdline(process):
     try:
@@ -14,27 +15,34 @@ def get_cmdline(process):
     except psutil.AccessDenied:
         return ('',)
 
+
+@caching.cache()
+def get_cwd(process):
+    try:
+        return process.cwd()
+    except psutil.AccessDenied:
+        return ('',)
+
+
 @click.command()
 @click.argument('search_str')
 @click.option('-k', '--kill', is_flag=True, default=False, help='Should kill the process')
 @click.option('-r', '--restart', is_flag=True, default=False, help='Should restart the process')
 def ps(search_str, kill, restart):
     python_processes = [process for process in psutil.process_iter() if re.match(r'^python[-.0-9]*w?(:?.exe)?$', process.name())]
-    processes = []
-
-    for process in python_processes:
-        if len(get_cmdline(process)) > 1 and search_str in get_cmdline(process)[1].lower():
-            processes.append(process)
+    processes = [process for process in python_processes
+                 if len(get_cmdline(process)) > 1 and search_str in get_cmdline(process)[1].lower()]
 
     for process in processes:
         cmdline = get_cmdline(process)
+        cwd = get_cwd(process)
         click.echo('found process:')
         click.echo(' '.join(cmdline))
         if kill or restart:
             click.echo('killing pid={0}'.format(process.pid))
             process.kill()
         if restart:
-            new = subprocess.Popen(cmdline)
+            new = subprocess.Popen(cmdline, cwd=cwd)
             click.echo('started new process pid={0}'.format(new.pid))
 
 
